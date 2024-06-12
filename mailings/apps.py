1c8ab89a46904f6bb35f207019f6a9c885484d5a
ystass <stepanischevyy@mail.ru>
2024-06-12 11:52:22 +0400

    verbose_name = "Маркетинговые рассылки"

--- mailings/apps.py
+++ mailings/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class MailingsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mailings"
+    verbose_name = "Маркетинговые рассылки"
+
