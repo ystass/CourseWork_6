4b57b477b471f66b9c13a43d37f79bae5f099209
ystass <stepanischevyy@mail.ru>
2024-06-12 11:53:24 +0400

    add admin

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailings.models import ServiceClient, MailingSettings
+
+
+@admin.register(ServiceClient)
+class ServiceClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'comment',)
+    list_filter = ('name', 'email',)
+
+
+@admin.register(MailingSettings)
+class MailingSettingsAdmin(admin.ModelAdmin):
+    list_display = ('client', 'title', 'periodicity', 'status',)
+    list_filter = ('client', 'title',)
