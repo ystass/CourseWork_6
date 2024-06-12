d61936815792b1826c17271b025ac57464662244
ystass <stepanischevyy@mail.ru>
2024-06-12 14:18:53 +0400

    GRUD для клиентов сервиса

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from mailings.apps import MailingsConfig
+from mailings.views import ServiceClientListView
 
 app_name = MailingsConfig.name
 
 urlpatterns = [
-
+    path('mailings/', ServiceClientListView.as_view(), name='mailings_list'),
 ]
