cbe7aec86c4b1ff062c21942965758ddd0d1fa1a
ystass <stepanischevyy@mail.ru>
2024-06-13 13:11:09 +0400

    создание контроллеров и форм

--- mailings/urls.py
+++ mailings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from mailings.apps import MailingsConfig
-from mailings.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView
+from mailings.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView, \
+    MailingSettingsListView, MailingSettingsDetailView, MailingSettingsCreateView, MailingSettingsUpdateView, \
+    MailingSettingsDeleteView
 
 app_name = MailingsConfig.name
 
@@ -11,4 +13,9 @@ urlpatterns = [
     path('mailings/create/', ClientCreateView.as_view(), name='client_create'),
     path('mailings/<int:pk>/update/', ClientUpdateView.as_view(), name='client_update'),
     path('mailings/<int:pk>/delete/', ClientDeleteView.as_view(), name='client_delete'),
+    path('mailings', MailingSettingsListView.as_view(), name='mailingsettings_list'),
+    path('mailingsettings/<int:pk>/', MailingSettingsDetailView.as_view(), name='mailingsettings_detail'),
+    path('mailingsettings/create/', MailingSettingsCreateView.as_view(), name='mailingsettings_create'),
+    path('mailingsettings/<int:pk>/update/', MailingSettingsUpdateView.as_view(), name='mailingsettings_update'),
+    path('mailingsettings/<int:pk>/delete/', MailingSettingsDeleteView.as_view(), name='mailingsettings_delete'),
 ]
--- mailings/views.py
+++ mailings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from mailings.models import Client
+from mailings.models import Client, MailingSettings
 
 
 class ClientListView(ListView):
@@ -28,3 +28,28 @@ class ClientUpdateView(UpdateView):
 class ClientDeleteView(DeleteView):
     model = Client
     success_url = reverse_lazy('mailings:client_list')
+
+
+class MailingSettingsListView(ListView):
+    model = MailingSettings
+
+
+class MailingSettingsDetailView(DetailView):
+    model = MailingSettings
+
+
+class MailingSettingsCreateView(CreateView):
+    model = MailingSettings
+    fields = ('start_time', 'end_time', 'periodicity', 'status', 'title', 'text')
+    success_url = reverse_lazy('mailings:mailingsettings_list')
+
+
+class MailingSettingsUpdateView(UpdateView):
+    model = MailingSettings
+    fields = ('start_time', 'end_time', 'periodicity', 'status', 'title', 'text')
+    success_url = reverse_lazy('mailings:mailingsettings_list')
+
+
+class MailingSettingsDeleteView(DeleteView):
+    model = MailingSettings
+    success_url = reverse_lazy('mailings:mailingsettings_list')
