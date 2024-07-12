9b64f8360810e028b68741b13f3de0b375b8eeea
ystass <stepanischevyy@mail.ru>
2024-07-12 13:35:06 +0400

    доступ для авторизованных пользователей

--- mailings/views.py
+++ mailings/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -12,19 +13,26 @@ class ClientDetailView(DetailView):
     model = Client
 
 
-class ClientCreateView(CreateView):
+class ClientCreateView(LoginRequiredMixin, CreateView):
+    model = Client
+    fields = ('name', 'email', 'comment')
+    success_url = reverse_lazy('mailings:client_list')
+
+    def form_valid(self, form):
+        client = form.save()
+        user = self.request.user
+        client.owner = user
+        client.save()
+        return super().form_valid(form)
+
+
+class ClientUpdateView(LoginRequiredMixin, UpdateView):
     model = Client
     fields = ('name', 'email', 'comment')
     success_url = reverse_lazy('mailings:client_list')
 
 
-class ClientUpdateView(UpdateView):
-    model = Client
-    fields = ('name', 'email', 'comment')
-    success_url = reverse_lazy('mailings:client_list')
-
-
-class ClientDeleteView(DeleteView):
+class ClientDeleteView(LoginRequiredMixin, DeleteView):
     model = Client
     success_url = reverse_lazy('mailings:client_list')
 
@@ -37,19 +45,26 @@ class MailingSettingsDetailView(DetailView):
     model = MailingSettings
 
 
-class MailingSettingsCreateView(CreateView):
+class MailingSettingsCreateView(LoginRequiredMixin, CreateView):
     model = MailingSettings
     fields = ('client', 'start_time', 'end_time', 'periodicity', 'status', 'title', 'text')
     success_url = reverse_lazy('mailings:mailingsettings_list')
 
+    def form_valid(self, form):
+        client = form.save()
+        user = self.request.user
+        client.owner = user
+        client.save()
+        return super().form_valid(form)
 
-class MailingSettingsUpdateView(UpdateView):
+
+class MailingSettingsUpdateView(LoginRequiredMixin, UpdateView):
     model = MailingSettings
     fields = ('start_time', 'end_time', 'periodicity', 'status', 'title', 'text')
     success_url = reverse_lazy('mailings:mailingsettings_list')
 
 
-class MailingSettingsDeleteView(DeleteView):
+class MailingSettingsDeleteView(LoginRequiredMixin, DeleteView):
     model = MailingSettings
     success_url = reverse_lazy('mailings:mailingsettings_list')
 
