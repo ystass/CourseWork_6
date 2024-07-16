d3d7b3bbb1dd80666ff7937432f740c6b21b7a72
ystass <stepanischevyy@mail.ru>
2024-07-16 17:21:38 +0400

    Реализовано приложение для ведения блога

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy, reverse
 
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
@@ -5,13 +6,13 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 from blog.models import Article
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(PermissionRequiredMixin, CreateView):
     model = Article
     fields = ('title', 'content', 'preview',)
     success_url = reverse_lazy('blog:blog_list')
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(PermissionRequiredMixin, UpdateView):
     model = Article
     fields = ('title', 'content', 'preview',)
 
@@ -33,6 +34,6 @@ class ArticleDetailView(DetailView):
         return self.object
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(PermissionRequiredMixin, DeleteView):
     model = Article
     success_url = reverse_lazy('blog:blog_list')
